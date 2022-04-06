018bd719c5eaa47e7df6181cc03c27f88bff8228
Adalia da Silva <ada.future.work@gmail.com>
2022-04-06 19:00:32 +0200

    add login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import SignUpForm from "./components/SignUpForm";
+import LogInForm from "./components/LogInForm";
 
 function App() {
   return (
-    <div className="App">
-      <SignUpForm />
-    </div>
+    <Routes>
+      <Route path="/" element={<LogInForm />} />
+      <Route path="/signup" element={<SignUpForm />} />
+
+{/*       <div className="App">
+        <SignUpForm />
+        <LogInForm />
+      </div> */}
+    </Routes>
   );
 }
 
