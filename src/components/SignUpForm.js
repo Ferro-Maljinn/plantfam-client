7d265f555a40d70424ec92bc20f7d71db1a48727
Adalia da Silva <ada.future.work@gmail.com>
2022-04-08 13:27:23 +0200

    fix API_URL => moved config file into src folder

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,7 +2,7 @@ import { Form, Input, Button, Checkbox } from "antd";
 import axios from "axios";
 import { useState, } from "react";
 import { useNavigate } from "react-router";
-import { API_URL } from "./config";
+import { API_URL } from "../config";
 
 const defaultFormState = {
   name: "",
