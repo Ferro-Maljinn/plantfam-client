f587cce1ecdb8bc500dbdb346063e04549383eab
Charlie Hölscher <charlieholscher@gmail.com>
2022-04-14 23:15:55 +0200

    profile stylings done

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,41 +1,55 @@
-
 // import AddPlantForm from "../components/AddPlantForm";
+import "./ProfilePage.css"
 
-export default function ProfilePage( { user, allPlants } ) {
+export default function ProfilePage({ user, allPlants }) {
+  //  let plantsOfUser
 
- 
-//  let plantsOfUser 
+  // if(allPlants){
+  // async function filterPlants() {
+  //   plantsOfUser = await allPlants.filter((plant) => {
+  //     return plant.owner === user._id
+  //   })
+  //   }
+  //   filterPlants();
+  //   console.log("plant & user", plantsOfUser)}
 
-    // if(allPlants){
-    // async function filterPlants() {
-    //   plantsOfUser = await allPlants.filter((plant) => {
-    //     return plant.owner === user._id
-    //   })
-    //   }
-    //   filterPlants(); 
-    //   console.log("plant & user", plantsOfUser)}
-  
-    
-  console.log(allPlants, "allplants from profilepage")
+  console.log(allPlants, "allplants from profilepage");
 
-  if(!user || !allPlants){
-    return <p>Loading...</p>
+  if (!user || !allPlants) {
+    return <p>Loading...</p>;
   }
   return (
-    <div >
-  
+    <div className="profile-container">
+    <div className="hello-profile" >
       <h1>Hello {user && user.name}</h1>
-      
-      {allPlants && allPlants.filter((plant) =>{
-        return plant.owner === user._id
-      })
-      .map((plant, i) => {
-        return <div key={plant.englishName + i} >
-        <img className="plant-img" src={plant.image} alt="Some Plant" />
-        <p> { plant.englishName } </p>
-        </div>
-      })
-      } 
       </div>
+      
+      {allPlants &&
+        allPlants
+          .filter((plant) => {
+            return plant.owner === user._id;
+          })
+          .map((plant, i) => {
+            return (
+              
+                <div className="outter-container">
+                  <div className="orgasm-border-gradient">
+                    <div className="plantDetails-andImg-container-profile">
+                      <img
+                        className="plant-img"
+                        src={plant.image}
+                        alt="Some Plant"
+                      />
+                       <div className="plant-details">
+                      <h4 key={plant.englishName + i} > {plant.englishName} </h4>
+                      <p className="plant-description">{plant.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+        
+            );
+          })}
+    </div>
   );
 }
