e5d028290bc89933b822fda194c010e62177bd70
Adalia da Silva <ada.future.work@gmail.com>
2022-04-14 02:51:22 +0200

    add css on PlantCard Component, replace buttons with icons, not finished

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from "react";
 import "./PlantCard.css";
-
-import { Button } from "antd";
+import { Button, Colors } from "react-foundation";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { API_URL } from "../config";
-import { useParams } from "react-router";
 
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 
 export default function PlantCard({ plant, setAllPlants }) {
-
   async function handleDeletePlant(plantId) {
     try {
       console.log("arrived to delete");
-     let deletedPlant = await axios.delete(`${API_URL}/plant/delete/${plantId}`, {
-        params: { plantId: plantId },
-      });
-      console.log(deletedPlant.data, "the deleted plant")
+      let deletedPlant = await axios.delete(
+        `${API_URL}/plant/delete/${plantId}`,
+        {
+          params: { plantId: plantId },
+        }
+      );
+      console.log(deletedPlant.data, "the deleted plant");
       setAllPlants(deletedPlant.data);
     } catch (err) {
       console.log("error from delet plants", err);
@@ -27,34 +26,56 @@ export default function PlantCard({ plant, setAllPlants }) {
   }
 
   return (
-    <div className="plant-card">
-      <img className="plant-img" src={plant.image} alt="Some Plant" />
-      <div className="plant-details">
-        <h2>{plant.englishName}</h2>
-        <p>{plant.description}</p>
+    <div className="orgasm-border-gradient">
+      <ButtonGroup className="button-group" bsSize="xsmall">
+        <Link to={`/plant/${plant._id}`}>
+          <div className="icon-container">
+            <img
+              src="/iconify/32x32/ant-design_edit-outlined.png"
+              alt={plant.englishName}
+            />
+            <h6>Edit</h6>
+          </div>
+        </Link>
+
+        <div className="icon-container">
+          <img
+            onClick={() => handleDeletePlant(plant._id)}
+            src="/iconify/32x32/ant-design_delete-outlined.png"
+            alt={plant.englishName}
+          />
+          <h6>Delete</h6>
+        </div>
+
+        <Link to={`/comments/${plant._id}`}>
+          <div className="icon-container">
+            <img
+              src="/iconify/32x32/codicon_comment-discussion.png"
+              alt={plant.englishName}
+            />
+            <h6>Comment</h6>
+          </div>
+        </Link>
+
+        <Link to={`/plantdetailspage/${plant._id}`}>
+          <div className="icon-container">
+            <img
+              src="/iconify/32x32/clarity_details-line.png"
+              alt={plant.englishName}
+            />
+            <h6>Details</h6>
+          </div>
+        </Link>
+        <hr />
+      </ButtonGroup>
+      
+      <div className="plantDetails-andImg-container">
+        <img className="plant-img" src={plant.image} alt="Some Plant" />
+        <div className="plant-details">
+          <h3>{plant.englishName}</h3>
+          <p>{plant.description}</p>
+        </div>
       </div>
-      <Link to={`/plant/${plant._id}`}>
-        {" "}
-        <Button type="primary" htmlType="submit">
-          Update
-        </Button>{" "}
-      </Link>
-      <Button
-        onClick={() => handleDeletePlant(plant._id)}
-        type="danger"
-        htmlType="submit"
-      >
-        Delete Plant
-      </Button>
-
-      <Link to={`/comments/${plant._id}`}>
-        <button>Comment</button>{" "}
-      </Link>
-
-      <Link to={`/plantdetailspage/${plant._id}`}>
-        <button>Details</button>{" "}
-      </Link>
-
     </div>
   );
 }
